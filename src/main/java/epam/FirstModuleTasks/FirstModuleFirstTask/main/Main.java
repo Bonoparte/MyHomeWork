8ca20da94510b767e386ab6630e7473c8e327f35
Frolov Anatoly <Frolov.an.m@yandex.ru>
2017-11-15 20:56:25 +0300

    Update

---
+++ src/main/java/epam/FirstModuleTasks/FirstModuleFirstTask/main/Main.java
@@ -0,0 +1,9 @@
+package main.java.epam.FirstModuleTasks.FirstModuleFirstTask.main;
+import main.java.epam.FirstModuleTasks.FirstModuleFirstTask.logic.Logic;
+
+public class Main {
+    public static void main(String[] args) {
+        Logic logic = new Logic();
+        System.out.println(logic.method());
+    }
+}
---
+++ src/main/java/epam/FirstModuleTasks/FirstModuleSecondTask.java
@@ -0,0 +1,7 @@
+package main.java.epam.FirstModuleTasks;
+
+public class FirstModuleSecondTask {
+    public static void main(String[] args) {
+        
+    }
+}
