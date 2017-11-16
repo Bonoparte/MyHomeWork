6accaba78a4b3803affa01d675e9443de26d53ce
Frolov Anatoly <Frolov.an.m@yandex.ru>
2017-11-16 06:11:30 +0300

    SecondModuleSecondTask

---
+++ src/main/java/epam/SecondModuleTasks/SecondModuleSecondTask/SecondModuleSecondTask.java
@@ -0,0 +1,16 @@
+package main.java.epam.SecondModuleTasks.SecondModuleSecondTask;
+
+public class SecondModuleSecondTask {
+    public static void main(String[] args) {
+        Worker firstWorker = new Worker();
+        Stationery pen = new Stationery("Pen", 16.30);
+        Stationery eraser = new Stationery("Eraser", 9.50);
+        Stationery pencil = new Stationery("Pencil", 22.40);
+        firstWorker.getStuff(pen);
+        firstWorker.getStuff(pen);
+        firstWorker.getStuff(eraser);
+        firstWorker.removeStuff(pencil);
+        firstWorker.showAllStuff();
+        firstWorker.showHowMuchCost();
+    }
+}
---
+++ src/main/java/epam/SecondModuleTasks/SecondModuleSecondTask/Stationery.java
@@ -0,0 +1,40 @@
+package main.java.epam.SecondModuleTasks.SecondModuleSecondTask;
+
+public class Stationery {
+    private String title;
+    private double price;
+
+    public Stationery() {
+        this.title = "Something that's no one knows how to call";
+        this.price = 20;
+    }
+
+    public Stationery(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Stationery{" +
+                "title='" + title + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
