82a53265a87b8b764bbc1459234f7c78e6ffd163
Frolov Anatoly <Frolov.an.m@yandex.ru>
2017-11-20 02:32:28 +0300

    FourthModuleThirdTask

---
+++ src/main/java/com/epam/FourthModuleTasks/FourthModuleThirdTask/FourthModuleThirdTask.java
@@ -0,0 +1,27 @@
+package com.epam.FourthModuleTasks.FourthModuleThirdTask;
+
+import java.io.*;
+
+public class FourthModuleThirdTask {
+    public static void main(String[] args) {
+        StringBuilder textSB = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("test.txt"),"UTF-8"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                textSB.append(line);
+                textSB.append('\n');
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedWriter bw = new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream(
+                        new File("outTest.txt")),"UTF-16"));) {
+                bw.write(textSB.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(textSB);
+    }
+}
