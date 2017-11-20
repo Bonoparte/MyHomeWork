7260b934dddeb7faf6b6970c8c973028cc0f157b
Frolov Anatoly <Frolov.an.m@yandex.ru>
2017-11-20 07:44:13 +0300

    update

--- src/main/java/com/epam/FifthModuleTasks/FifthModuleSecondTask/FifthModuleSecondTask.java
+++ src/main/java/com/epam/FifthModuleTasks/FifthModuleSecondTask/FifthModuleSecondTask.java
@@ -2,7 +2,7 @@ package com.epam.FifthModuleTasks.FifthModuleSecondTask;
 
 public class FifthModuleSecondTask {
     public static void main(String[] args) {
-        PropertiesGetter propertiesGetter = new PropertiesGetter("firstKejky");
+        PropertiesGetter propertiesGetter = new PropertiesGetter("firstKey");
 //        System.out.println(propertiesGetter);
     }
 }
